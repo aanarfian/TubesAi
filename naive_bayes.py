da54a2f6cd834c835f0ec4041e636c13be2b6e30
fadhlimulyana20 <baba.fadhli@gmail.com>
2021-06-28 06:41:11 +0700

    Menambahkan naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,5 +1,35 @@
-from math import sqrt
+from csv import reader
+from math import exp, pi, sqrt
 
+# Load a CSV file
+def load_csv(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+        print('[%s] => %d' % (value, i))
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+# Split the dataset by class values, returns a dictionary
 def separate_by_class(dataset):
     separated = dict()
     for i in range(len(dataset)):
@@ -34,19 +64,59 @@ def summarize_by_class(dataset):
 		summaries[class_value] = summarize_dataset(rows)
 	return summaries
 
-# Test separating data by class
-dataset = [[3.393533211, 2.331273381, 0],
-           [3.110073483, 1.781539638, 0],
-           [1.343808831, 3.368360954, 0],
-           [3.582294042, 4.67917911, 0],
-           [2.280362439, 2.866990263, 0],
-           [7.423436942, 4.696522875, 1],
-           [5.745051997, 3.533989803, 1],
-           [9.172168622, 2.511101045, 1],
-           [7.792783481, 3.424088941, 1],
-           [7.939820817, 0.791637231, 1]]
-summary = summarize_by_class(dataset)
-for label in summary:
-	print(label)
-	for row in summary[label]:
-		print(row)
+
+# Calculate the Gaussian probability distribution function for x
+def calculate_probability(x, mean, stdev):
+	exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
+	return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+# Calculate the probabilities of predicting each class for a given row
+def calculate_class_probabilities(summaries, row):
+	total_rows = sum([summaries[label][0][2] for label in summaries])
+	probabilities = dict()
+	for class_value, class_summaries in summaries.items():
+		probabilities[class_value] = summaries[class_value][0][2]/float(total_rows)
+		for i in range(len(class_summaries)):
+			mean, stdev, _ = class_summaries[i]
+			probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
+	return probabilities
+
+def predict(summaries, row):
+    probabilities = calculate_class_probabilities(summaries, row)
+    best_label, best_prob = None, -1
+    for class_value, probability in probabilities.items():
+        if best_label is None or probability > best_prob:
+            best_prob = probability
+            best_label = class_value
+    return best_label
+
+# # Test separating data by class
+# dataset = [[3.393533211, 2.331273381, 0],
+#            [3.110073483, 1.781539638, 0],
+#            [1.343808831, 3.368360954, 0],
+#            [3.582294042, 4.67917911, 0],
+#            [2.280362439, 2.866990263, 0],
+#            [7.423436942, 4.696522875, 1],
+#            [5.745051997, 3.533989803, 1],
+#            [9.172168622, 2.511101045, 1],
+#            [7.792783481, 3.424088941, 1],
+#            [7.939820817, 0.791637231, 1]]
+# summaries = summarize_by_class(dataset)
+# probabilities = calculate_class_probabilities(summaries, dataset[3])
+# print(probabilities)
+
+filename = 'dataset.csv'
+dataset = load_csv(filename)
+
+for i in range(len(dataset[0])-1):
+    str_column_to_float(dataset, i)
+
+str_column_to_int(dataset, len(dataset[0])-1)
+# fit model
+model = summarize_by_class(dataset)
+# define a new record
+row = [5.7,2.9,4.2,1.3]
+# predict the label
+label = predict(model, row)
+print('Data=%s, Predicted: %s' % (row, label))
+
