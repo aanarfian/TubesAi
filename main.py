808e5c167396500704b0133c5897e49ffe2a9e4f
F-Pawamatra <72060401+F-Pawamatra@users.noreply.github.com>
2021-06-27 14:57:51 +0700

    Shape detection

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('apelAI.jpg', 0)
-gblur = cv2.GaussianBlur(img, (11, 11), 0)
-_, th1 = cv2.threshold(gblur, 230, 255, cv2.THRESH_BINARY)
-# kernel = np.ones((5, 5), np.float32)/25
+img = cv2.imread('apelAI.jpg')
+imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
+contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+cv2.imshow("img", img)
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+    cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1] - 5
+    if len(approx) == 3:
+        cv2.putText(img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    elif len(approx) == 4:
+        x1 ,y1, w, h = cv2.boundingRect(approx)
+        aspectRatio = float(w)/h
+        print(aspectRatio)
+        if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+          cv2.putText(img, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+        else:
+          cv2.putText(img, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    elif len(approx) == 5:
+        cv2.putText(img, "Pentagon", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    elif len(approx) == 10:
+        cv2.putText(img, "Star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    else:
+        cv2.putText(img, "Circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
 
 
-cv2.imshow("Image", th1)
+cv2.imshow("shapes", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+# Gaussian blur
+
+
+# img = cv2.imread('apelAI.jpg', 0)
+# gblur = cv2.GaussianBlur(img, (11, 11), 0)
+# _, th1 = cv2.threshold(gblur, 230, 255, cv2.THRESH_BINARY)
+# # kernel = np.ones((5, 5), np.float32)/25
+
+
+# cv2.imshow("Image", th1)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+
+
+
 
 # import cv2
 # import numpy as np
