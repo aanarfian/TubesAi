f229308f308b9487b2ea4a2153940b0c18f19d54
fadhlimulyana20 <baba.fadhli@gmail.com>
2021-06-28 17:07:46 +0700

    Menambahkan endpoint upload file

--- ui.py
+++ ui.py
@@ -1,11 +1,44 @@
-from flask import Flask, redirect, url_for, render_template
+import os
+from flask import Flask,jsonify, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = os.path.join('asset', 'upload')
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def home():
-        return render_template("index.html", content="Testing")
+	return render_template("index.html", content="Testing")
+
+@app.route('/file-upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are png, jpg'})
+		resp.status_code = 400
+		return resp
 
 if __name__ == "__name_-":
     app.run()
