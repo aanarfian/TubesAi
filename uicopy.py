cd741295e66bb2aba4f4c361357e3b7b1641afb3
fadhlimulyana20 <baba.fadhli@gmail.com>
2021-06-29 13:09:26 +0700

    Menmabahkan upload

--- uicopy.py
+++ uicopy.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask,jsonify, render_template, request, redirect
+from flask import Flask,jsonify, render_template, request, redirect, send_from_directory
 from werkzeug.utils import secure_filename
 import testui
 import cv2
+import uuid
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = os.path.join('asset', 'upload')
+app.config['GBLUR_FOLDER'] = os.path.join('asset', 'gblur')
 
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg'])
@@ -25,16 +27,20 @@ def home():
 	matang = 0
 	mentah = 0
 	klasifikasi = 0
+	img = None
+	gblur = None
+	new_filename = ModuleNotFoundError
 	if request.method == "POST":
 		if request.files:
 			image = request.files["file"]
-			# print(image)
-			image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
-			img = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], image.filename, 1))
-			gray = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], image.filename, 0))
+			_, file_ext = os.path.splitext(str(image.filename))
+			new_filename = ''.join((str(uuid.uuid1()), file_ext))
+			image.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
+			gray = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], new_filename), 0)
 			gblur = cv2.GaussianBlur(gray, (5, 5), 0)
+			cv2.imwrite(os.path.join(app.config['GBLUR_FOLDER'], new_filename), gblur)
 
-			imageData, classified, matangmentah, lookup = testui.process_image(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+			imageData, classified, matangmentah, lookup = testui.process_image(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
 			if lookup["matang"]==matangmentah[0]:
 				klasifikasi = "Mateng"
 				matang = matangmentah[0]
@@ -59,7 +65,15 @@ def home():
 			smoothness = imageData[4]
 
 	# print(energy)
-	return render_template("index.html", content=[energy, entropy, intensitas, kontras, smoothness, matang, mentah, klasifikasi, img, gblur])
+	return render_template("index.html", content=[energy, entropy, intensitas, kontras, smoothness, matang, mentah, klasifikasi, img, gblur, new_filename])
+
+@app.route('/asset/upload/<name>')
+def upload_image(name):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], name, as_attachment=False)
+
+@app.route('/asset/gblur/<name>')
+def gblur_image(name):
+	return send_from_directory(app.config['GBLUR_FOLDER'], name, as_attachment=False)
 
 @app.route('/file-upload', methods=['POST'])
 def upload_file():
